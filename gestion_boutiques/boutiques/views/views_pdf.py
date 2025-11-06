1780751ccede1f2499e7227fbb9bdfb9788d3add
hugues <hugueswilson16@gmail.com>
2025-11-06 01:45:40 +0100

    commit

--- gestion_boutiques/boutiques/views/views_pdf.py
+++ gestion_boutiques/boutiques/views/views_pdf.py
@@ -17,21 +17,16 @@ class VentePDFView(DetailView):
     """Vue qui génère le PDF d'une vente"""
     model = Vente
     template_name = "PDF/PDF_vente.html"  # Ton template HTML
-
     def render_to_pdf(self, context):
         # Rendre le template HTML en string
         html_string = render_to_string(self.template_name, context)
-
         # Convertir en PDF avec WeasyPrint
         pdf_file = HTML(string=html_string, base_url=self.request.build_absolute_uri()).write_pdf()
-
         return pdf_file
-
     def get(self, request, *args, **kwargs):
         vente = self.get_object()
         context = {"vente": vente}
         pdf_file = self.render_to_pdf(context)
-
         if not pdf_file:
             return HttpResponse("Erreur lors de la génération du PDF", status=500)
         response = HttpResponse(pdf_file, content_type="application/pdf")
